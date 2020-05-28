822acf3631f3739a4cc4ec620c5b0116134a23bd
EdoFro <35700575+EdoFro@users.noreply.github.com>
2020-05-28 13:42:43 -0400

    little corrections

--- copy node link to clipboard/copyLinkToClipboard.groovy
+++ copy node link to clipboard/copyLinkToClipboard.groovy
@@ -11,16 +11,13 @@ Script to easy copy the link to a file of a node into the clipboard to paste it 
 
 def texto // defining variable texto 
 
-/* forma anterior
-texto = node.link.text.replace('%20',' ')
-*/
 texto = node.link.text //obtaining the link text from the selected node
 texto = URLDecoder.decode(texto) // decoding it to be "windows readable"
-/* forma anterior (other way I used before. didn't notice any diference)
+/* forma anterior (other way I used before to decode. didn't notice any diference)
 texto = node.link.text.replace('%20',' ')
 */
 
-//looks if the text satarts with 'file:/'
+//looks if the text starts with 'file:/'
 if (texto[0..5]=='file:/'){
     texto = texto.drop(6).replace('/','\\') //replaces slashes with backslashes
 }
